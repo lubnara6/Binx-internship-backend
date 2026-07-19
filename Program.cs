1edfcb16698245574b7cc0d4a3dfefc1a286296f
Lubna <lubnaragaby@gmail.com>
2026-07-19 11:37:59 +0100

    Day 1: HelloBinX console application

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+
+class Program
+{
+
+
+
+    static void Main(string[] args)
+
+    {
+        Console.WriteLine("My name is Lubna");
+        Console.WriteLine("Today's date is {19/7/2026}");
+       
+      
+
+   }
+} 
