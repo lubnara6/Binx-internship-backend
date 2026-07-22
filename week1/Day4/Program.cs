c2bdaed4647bc63c0599253545b7c1e00ffbea08
Lubna <lubnaragaby@gmail.com>
2026-07-22 18:27:25 +0100

    Add Day 4 Collections and LINQ lab

---
+++ week1/Day4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    Dictionary<int,string> names = new Dictionary<int, string>();
+    names.Add(989,"lubna");
+    names.Add(5454,"Ali");
+    names.Add(984,"Samer");
+
+    foreach(var item in names)
+        {
+            Console.WriteLine($"the key value is{item.Key}");
+        }
+
+
+
+
+    }
+}
